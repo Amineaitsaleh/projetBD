af684115baedbad270267a733f1e742e73eaeb8d
Aimran Alyachiri <lyaaimran@gmail.com>
2025-05-31 15:04:47 +0100

    Modifier

--- index.py
+++ index.py
@@ -12,6 +12,7 @@ with st.sidebar:
     choix = st.radio("", ["Accueil", "Liste des Clients", "Liste des Réservations","Les chambres dispinible", "Ajouter Client","Ajouter Réservation"])
 
 data = sqlite3.connect("GrHotel.db")
+sqlite3.connect("PARGMA foreign_keys =ON ")
 
 if choix == "Accueil":
     st.header(" Bienvenue à l'application de gestion")
@@ -84,5 +85,55 @@ elif choix== "Ajouter Client":
                 )
                 data.commit()
                 st.success("Client ajouté avec succès!")
+elif choix== "Liste des Réservations": 
+    st.header("La liste des Reservations :") 
+    reservation=pd.read_sql_query("SELECT * FROM Reservation",data)
+    st.dataframe(reservation)
 
+elif choix== "Ajouter Réservation":   
+    st.title('Ajouter Réservation')  
+
+    with st.form('Ajouter_Reser_form'):
+            Nomhotel=st.selectbox('Nom De hotel',["-- Veuillez choisir une Hotel --","Mariaf","Monalissa","Mogador"])
+            typeChambre=st.selectbox('Type de Chambre',["-- Veuillez choisir une Type de Chambre --","Simple","Double"])
+            fumeurs=st.selectbox('Fumeurs',["NON","OUI"])
+            date_debut=st.date_input('date debut')
+            date_fin=st.date_input('date fin')
+            Id_client=st.text_input('CNE')
+            submitted = st.form_submit_button("Ajouter")
+
+            if submitted:
+                erreurs = []
+                if Nomhotel=='-- Veuillez choisir une Hotel --':
+                  erreurs.append("Vous devez sélectionner un Nom de Hotel valide")
+                if typeChambre=='-- Veuillez choisir une Type de Chambre --':
+                   erreurs.append("Vous devez sélectionner un Type de Chambre valide")
+                if not date_debut:
+                     erreurs.append("L'adresse est requise.")
+                if not date_fin:
+                        erreurs.append("L'adresse est requise.")
+                if not Id_client:
+                       erreurs.append("CNE est requise.")
+                if erreurs:
+                    for i in erreurs:
+                        st.error(i)
+                else:
+                    cur = data.cursor()
+                    fumeurs=1 if fumeurs=='OUI' else 0
+                    cur.execute("""
+                         SELECT C.Id_Chambre
+                         FROM Typechambre TC
+                         JOIN Chambre C ON TC.Id_type = C.Id_type
+                         WHERE TC.Type = ? AND C.fumeurs = ?
+                        """, (typeChambre, fumeurs))
+                    result = cur.fetchone()
+                    if result is None:
+                         st.error("")
+                    else:
+                      IdCh=result[0]
+                      cur.execute(
+                      "INSERT INTO Reservation(NomHotel,Date_arrivee,Date_depart,Id_client,Id_Chambre,fumeurs)  VALUES ( ?, ?, ?, ?, ?,?)",
+                      (Nomhotel, date_debut, date_fin, Id_client, IdCh, fumeurs))
+                      data.commit()
+                      st.success("Client ajouté avec succès!")
 data.close()
--- main.py
+++ main.py
@@ -5,12 +5,14 @@ cursor = data.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Hotel(
-    Id_Hotel TEXT PRIMARY KEY,
+    Id_Hotel TEXT PRIMARY KEY,  
     Ville TEXT,
     Pays TEXT,
+    NomHotel TEXT,
     CodePostal INTEGER
 );
 """)
+
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS TypeChambre(
@@ -48,12 +50,18 @@ CREATE TABLE IF NOT EXISTS Client(
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Reservation(
-    Id_Reservation TEXT PRIMARY KEY,
+    Id_Reservation INTEGER PRIMARY KEY AUTOINCREMENT,
+    NomHotel Text,
     Date_arrivee DATE,
     Date_depart DATE,
     Id_client TEXT,
-    CONSTRAINT fk_Idclient FOREIGN KEY (Id_client) REFERENCES Client(Id_client)
-);
+    Id_Chambre TEXT,
+    fumeurs INTEGER,
+    CONSTRAINT fk_Idclient FOREIGN KEY (Id_client) REFERENCES Client(Id_client),
+    CONSTRAINT fk_NomHotel FOREIGN KEY (NomHotel) REFERENCES Hotel( NomHotel) ,
+    CONSTRAINT fk_IdChambre FOREIGN KEY (Id_Chambre) REFERENCES Chambre(Id_Chambre)  ,
+    CONSTRAINT fk_fumeurs FOREIGN KEY (fumeurs) REFERENCES Chambre(fumeurs)             
+);  
 """)
 
 cursor.execute("""
@@ -99,10 +107,11 @@ CREATE TABLE IF NOT EXISTS Offre(
 
 #Insertion
 Hotel=[
-    (1, 'Paris', 'France', 75001) ,
-    (2, 'Lyon', 'France', 69002),
+    ( 1,'Paris', 'France',"SANFRANCISSCO", 75001) ,
+    (2,'Lyon', 'France',"MONALISA",69002),
+    (3,'Lyon', 'France',"THE PLAZA",69002),
     ]
-cursor.executemany("INSERT INTO Hotel VALUES(?,?,?,?)",Hotel)
+#cursor.executemany("INSERT INTO Hotel VALUES(?,?,?,?,?)",Hotel)
 Client=[
     (1, '12 Rue de Paris', 'Paris', '75001', 'jean.dupont@email.fr', 
       '0612345678', 'Jean Dupont'),
@@ -115,7 +124,7 @@ Client=[
     (5, '3 Rue des Fleurs', 'Nice', '06000', 'emma.giraud@email.fr', 
     '0656789012', 'Emma Giraud') 
 ]
-cursor.executemany("INSERT INTO Client Values(?,?,?,?,?,?,?)",Client)
+#cursor.executemany("INSERT INTO Client Values(?,?,?,?,?,?,?)",Client)
 Prestation=[
     (1, 15, 'Petit-déjeuner') ,
     (2, 30, 'Navette aéroport'), 
@@ -123,12 +132,12 @@ Prestation=[
     (4, 50, 'Spa et bien-être'), 
     (5, 20, 'Parking sécurisé') 
 ]
-cursor.executemany("INSERT INTO Préstation Values(?,?,?)",Prestation)
+#cursor.executemany("INSERT INTO Préstation Values(?,?,?)",Prestation)
 TypeChambre=[
    (1, 'Simple', 80),
    (2, 'Double', 120)
 ]
-cursor.executemany("INSERT INTO TypeChambre Values(?,?,?)",TypeChambre)
+#cursor.executemany("INSERT INTO TypeChambre Values(?,?,?)",TypeChambre)
 Chambre=[
     (1, 201, 2, 0, 1, 1) ,
     (2, 502, 5, 1, 1, 2) ,
@@ -139,23 +148,24 @@ Chambre=[
     (7, 307, 3, 1, 1, 2) ,
     (8, 101, 1, 0, 1, 1) 
 ]
-cursor.executemany("INSERT INTO Chambre Values(?,?,?,?,?,?)",Chambre)
+#cursor.executemany("INSERT INTO Chambre Values(?,?,?,?,?,?)",Chambre)
 Reservation=[
     # Client 1 (Jean Dupont) 
-(1, '2025-06-15', '2025-06-18', 1) ,
+("SANFRANCISSCO", '2025-06-15', '2025-06-18',1,2,1) ,
 # Client 2 (Marie Leroy) 
-(2, '2025-07-01', '2025-07-05', 2) ,
-(7, '2025-11-12', '2025-11-14', 2) ,
-(10, '2026-02-01', '2026-02-05', 2) ,
+("MONALISA",'2025-07-01', '2025-07-05', 2,2,1) ,
+("THE PLAZA", '2025-11-12', '2025-11-14', 2,8,1) ,
+("MONALISA",'2026-02-01', '2026-02-05', 2,7,0) ,
 # Client 3 (Paul Moreau) 
-(3, '2025-08-10', '2025-08-14', 3) ,
+("SANFRANCISSCO", '2025-08-10', '2025-08-14', 3,4,1) ,
 # Client 4 (Lucie Martin) 
-(4, '2025-09-05', '2025-09-07', 4) ,
-(9, '2026-01-15', '2026-01-18', 4) ,
+("THE PLAZA", '2025-09-05', '2025-09-07', 4,7,1) ,
+("MONALISA", '2026-01-15', '2026-01-18', 4,8,0) ,
 # Client 5 (Emma Giraud) 
-(5, '2025-09-20', '2025-09-25', 5),
+("SANFRANCISSCO",'2025-09-20', '2025-09-25', 5,1,0),
 ]
-cursor.executemany("INSERT INTO Reservation Values(?,?,?,?)",Reservation)
+#cursor.executemany("INSERT INTO Reservation (NomHotel,Date_arrivee,Date_depart,Id_client,Id_Chambre,fumeurs) VALUES (?, ?, ?, ?, ?, ?);",Reservation)
+
 Evaluation=[
 (1, '2025-06-15', 5, 'Excellent séjour, personnel très accueillant.', 1, 2),
 (2, '2025-07-01', 4, 'Chambre propre, bon rapport qualité/prix.', 2, 1),
@@ -163,7 +173,7 @@ Evaluation=[
 (4, '2025-09-05', 5, 'Service impeccable, je recommande.', 4, 2),
 (5, '2025-09-20', 4, 'Très bon petit-déjeuner, hôtel bien situé.', 5, 1)
 ]
-cursor.executemany("INSERT INTO Evaluation Values(?,?,?,?,?,?)",Evaluation)
+#cursor.executemany("INSERT INTO Evaluation Values(?,?,?,?,?,?)",Evaluation)
 
 Concerner=[
     (1,1),
@@ -175,7 +185,9 @@ Concerner=[
     (7,9),
     (8,5)
 ]
-cursor.executemany("INSERT INTO Concerner Values(?,?)",Concerner)
+#cursor.executemany("INSERT INTO Concerner Values(?,?)",Concerner)
+
+
 
 
 data.commit()
